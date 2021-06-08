18ac13319eabafa6d97ba7ebbe72ff0f72f41a27
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-06-08 03:22:15 -0300

    implementing pomodoro, with bugs

--- toDo.js
+++ toDo.js
@@ -1,77 +1,107 @@
 import readlineSync from 'readline-sync';
+import fs from 'fs'
 
-const listToDo = [{done: false, text: "codar", symbol:"🔴"}, {done: true, text: "comer", symbol:"🟢"}, {done: true, text: "dormir", symbol:"🟢"}]
-let options = ['add', 'list', 'check', 'remove' ]
+let listToDo = []
+let options = ['add', 'list', 'check', 'remove', 'pomodoro' ]
 
 function toDo() {
-  const optionIndex = readlineSync.keyInSelect(options, "Type your commands");
+    if(fs.existsSync("./toDo.txt")) {
+        listToDo = JSON.parse(fs.readFileSync("./toDo.txt"))
+    }
+    const optionIndex = readlineSync.keyInSelect(options, "Type your commands");
 
   if (options[optionIndex] === "add") {
     add();
-    toDo();
-
   } else if (options[optionIndex] === "list") {
-    if (listToDo.length === 0) {
-      console.log("Não há nada a listar");
-      toDo();
-    }
     list();
-    toDo();
-
   } else if (options[optionIndex] === "check") {
-    if (listToDo.length === 0) {
-      console.log("Não há nada a checar");
-      toDo();
-    }
     check();
-    toDo();
-
   } else if (options[optionIndex] === "remove") {
-    if (listToDo.length === 0) {
-      console.log("Não há nada a deletar");
-      toDo();
-    }
     remove();
-    toDo();
+  } else if (options[optionIndex] === "pomodoro") {
+    pomodoro();
   }
 
   function add() {
     const addToDo = readlineSync.question("What do you want to do? ");
-    listToDo.push({ done: false, text: addToDo });
+    listToDo.push({ done: false, text: addToDo, symbol: "🔴", pomodoro: 0 });
+    fs.writeFileSync( "./toDo.txt", JSON.stringify(listToDo))
+    toDo();
   }
 
   function list() {
+    if (listToDo.length < 1) {
+      console.log("Não há nada a listar");
+      toDo();
+    }
     console.log("===========================================\n");
-    listToDo.map((i) => console.log(`${i.symbol} ${i.text}`));
-    console.log("\n===========================================");
+    listToDo.map((i) => {
+      console.log(`${i.symbol} ${i.text} ${"🍅".repeat(i.pomodoro)}` );
+      console.log("");
+    });
+    console.log("===========================================");
+    toDo();
   }
 
   function check() {
+    if (listToDo.length < 1) {
+      console.log("Não há nada a checar");
+      toDo();
+    }
     const checkIndex = readlineSync.keyInSelect(
-      listToDo.map((t) => `${t.symbol} ${t.text}`),
+      listToDo.map((t) => `${t.symbol} ${t.text} ${"🍅".repeat(t.pomodoro)}`),
       "Type your commands"
     );
     if (checkIndex >= 0) {
-        if(listToDo[checkIndex].done === true){
-            listToDo[checkIndex].done = !listToDo[checkIndex].done;
-            listToDo[checkIndex].symbol = "🔴"
-        } else {
-            listToDo[checkIndex].done = !listToDo[checkIndex].done;
-            listToDo[checkIndex].symbol = "🟢"
-        }
-
+      if (listToDo[checkIndex].done === true) {
+        listToDo[checkIndex].done = !listToDo[checkIndex].done;
+        listToDo[checkIndex].symbol = "🔴";
+      } else {
+        listToDo[checkIndex].done = !listToDo[checkIndex].done;
+        listToDo[checkIndex].symbol = "🟢";
+      }
+      check();
+    } else {
+      toDo();
     }
   }
 
   function remove() {
+    if (listToDo.length < 1) {
+      console.log("Não há nada a deletar");
+      toDo();
+    }
     const removeIndex = readlineSync.keyInSelect(
-      listToDo.map((t) => t.text),
+      listToDo.map((t) => `${t.symbol} ${t.text} ${"🍅".repeat(t.pomodoro)}`),
       "Type your commands"
     );
-    if (removeIndex >= 0) {
+    if (removeIndex > -1) {
       listToDo.splice(removeIndex, 1);
+      fs.writeFileSync( "./toDo.txt", JSON.stringify(listToDo))
+      remove();
+    } else {
+      toDo();
     }
   }
+}
+
+function pomodoro() {
+  if (listToDo.length < 1) {
+    console.log("Não há nada para usar o método de pomodoro");
+    toDo();
+  }
+  const pomodoroIndex = readlineSync.keyInSelect(
+    listToDo.map((t) => `${t.symbol} ${t.text} ${"🍅".repeat(t.pomodoro)}`),
+    "Type your commands"
+  );
+  setInterval(() => {
+      console.log(pomodoroIndex)
+    console.log(listToDo[pomodoroIndex].pomodoro);
+    listToDo[pomodoroIndex].pomodoro += 1;
+    toDo()
+  }, 1000);
+  console.log(`Pomodoro de "${listToDo[pomodoroIndex].text}" setado!`);
+
 }
 
 toDo()
